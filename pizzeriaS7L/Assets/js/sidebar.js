2fc8713dbaf22a8becc77e3fb197dea6274b13ae
Gaetano <gaem195@gmail.com>
2024-03-13 11:35:42 +0100

    Add functioning sidebar

---
+++ pizzeriaS7L/Assets/js/sidebar.js
@@ -0,0 +1,16 @@
+﻿const menuToggle = document.querySelector(".menuToggle");
+const sidebar = document.querySelector(".sidebar");
+
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle("active");
+    sidebar.classList.toggle("active");
+}
+
+const menuList = document.querySelectorAll(".menuList li")
+
+function activeLink() {
+    menuList.forEach(li => li.classList.remove("active"))
+    this.classList.add("active")
+}
+
+menuList.forEach(li => li.addEventListener("click", activeLink))
