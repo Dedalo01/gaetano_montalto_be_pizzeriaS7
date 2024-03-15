dd5b7232be83f998022174de53b0b9e596430c8d
Gaetano <gaem195@gmail.com>
2024-03-15 01:21:01 +0100

    add all functionalities

--- pizzeriaS7L/Assets/js/sidebar.js
+++ pizzeriaS7L/Assets/js/sidebar.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menuToggle");
     const sidebar = document.querySelector(".sidebar");
 
+    // Switcha tra classe attiva e non attiva
     menuToggle.onclick = function () {
-        menuToggle.classList.toggle("active");
-        sidebar.classList.toggle("active");
+        const isSidebarActive = sidebar.classList.contains("active");
+        menuToggle.classList.toggle("active", !isSidebarActive);
+        sidebar.classList.toggle("active", !isSidebarActive);
         // Salva stato sidebar quando viene cliccato il toggle
         saveSidebarState();
     }
-
+ 
     // Ripristina stato sidebar quando viene caricata pagina
-    restoreSidebarState();
-
+        restoreSidebarState();
+    
+    // Controlla url per lasciare attivo il giusto link
     const currentUrl = window.location.href;
-
     const menuList = document.querySelectorAll(".menuList li")
 
     if (menuList) {
@@ -25,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const anchor = link.querySelector("a")
             if (anchor) {
                 const href = anchor.getAttribute("href");
-                console.log(href)
+              
                 if (currentUrl.endsWith(href)) {
                     link.classList.add("active");
                 }
@@ -38,16 +40,16 @@ document.addEventListener("DOMContentLoaded", function () {
     function saveSidebarState() {
         const isSidebarOpen = sidebar.classList.contains("active");
         sessionStorage.setItem("sidebarState", isSidebarOpen ? "open" : "closed");
-        console.log(sessionStorage.getItem("sidebarState"));
+       
     }
 
-    // Funzione per ripristinare lo stato della sidebar dal session storage
+    // Ripristina lo stato della sidebar dal session storage
     function restoreSidebarState() {
         const sidebarState = sessionStorage.getItem("sidebarState");
         if (sidebarState === "open") {
             sidebar.classList.add("active");
             menuToggle.classList.add("active");
-            console.log(sessionStorage.getItem("sidebarState"));
+            
         }
     };
 });
